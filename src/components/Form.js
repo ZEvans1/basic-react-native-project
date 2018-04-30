9313b8f9fddeaf7b1f6f54c8615903876222fb08
zach <zte.zachary@gmail.com>
2018-04-29 21:26:28 -0700

    Add a password field and a touchable opacity for logging in.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,24 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 export default class Logo extends Component<{}> {
   render(){
     return(
       <View style={styles.container}>
-        <TextInput style={styles.inputBox}/>
+        <TextInput style={styles.inputBox}
+          underlineColorAndroid='rgba(0,0,0,0)'
+          placeholder="Email"/>
+        <TextInput style={styles.inputBox}
+          underlineColorAndroid='rgba(0,0,0,0)'
+          placeholder="Password"/>
+
+        <TouchableOpacity>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -25,5 +35,14 @@ const styles = StyleSheet.create({
 
   inputBox: {
     width: 300,
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    borderRadius: 25,
+    paddingHorizontal:16,
+    marginVertical: 10
+  },
+
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '500'
   }
 });
