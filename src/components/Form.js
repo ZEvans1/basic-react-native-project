053a50c2c9a698b738734ed29af10490f715614f
zach <zte.zachary@gmail.com>
2018-04-29 21:01:05 -0700

    Add a form input field.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,16 +3,14 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  TextInput
 } from 'react-native';
 
 export default class Logo extends Component<{}> {
   render(){
     return(
       <View style={styles.container}>
-        <Image source={{uri: 'https://facebook.github.io/react/logo-og.png'}}
-         style={{width: 100, height: 100}} />
-         <Text style={styles.logoText}>A Basic React Native App</Text>
+        <TextInput style={styles.inputBox}/>
       </View>
     )
   }
@@ -24,9 +22,8 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems: 'center'
   },
-  logoText: {
-    marginVertical: 18,
-    fontSize: 18,
-    color:'rgba(255, 255, 255, 0.7)'
+
+  inputBox: {
+    width: 300,
   }
 });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,12 +7,14 @@ import {
 } from 'react-native';
 
 import Logo from '../components/Logo';
+import Form from '../components/Form';
 
 export default class Login extends Component<{}> {
   render() {
     return(
       <View style={styles.container}>
         <Logo/>
+        <Form/>
       </View>
     )
   }
